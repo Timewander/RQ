ece65a0e149bdf4cbe6ec02a6a9e6a98f684024c
Timewander <sky.gao@d1m.cn>
2017-09-18 13:59:55 +0800

    update Http

--- init.php
+++ init.php
@@ -3,6 +3,7 @@
 define("ROOT_DIR", __DIR__);
 define("ENV", __DIR__ . "/env.php");
 define("RESOURCE", __DIR__ . "/resource.php");
+define("LOG_DIR", __DIR__ . '/storage');
 date_default_timezone_set("Asia/Shanghai");
 
 include "Init/autoload.php";
